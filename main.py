979714f4905afe6c0d0246a9b708e80dd08104e4
paolobertellini <225184@studenti.unimore.it>
2020-04-23 17:03:23 +0200

    Painting detection

--- main.py
+++ main.py
@@ -3,14 +3,21 @@ import os
 from paintingDetection import paintingDetection
 
 paolo_path = 'C:/VCS-project/VCS-project/Project material/videos/all/'
-path = paolo_path
+dav_path = '/media/davide/aukey/progetto_vision/videos/'
+
+path = dav_path
 videos = os.listdir(path)
 
-# canny trackbars
+# trackbars
 def nothing(x): pass
 cv2.namedWindow("Trackbars")
-cv2.createTrackbar("Th1", "Trackbars", 50, 500, nothing)
-cv2.createTrackbar("Th2", "Trackbars", 30, 500, nothing)
+cv2.createTrackbar("Hl", "Trackbars", 0, 400, nothing)
+cv2.createTrackbar("Sl", "Trackbars", 0, 400, nothing)
+cv2.createTrackbar("Vl", "Trackbars", 0, 400, nothing)
+cv2.createTrackbar("Hg", "Trackbars", 255, 255, nothing)
+cv2.createTrackbar("Sg", "Trackbars", 255, 255, nothing)
+cv2.createTrackbar("Vg", "Trackbars", 255, 255, nothing)
+
 
 for videoFile in videos:
 
@@ -23,14 +30,14 @@ for videoFile in videos:
 
         if frame is not None:
             frame_counter += 1
-            edges, frame = paintingDetection(frame, video)
+
+            frame = paintingDetection(frame)
 
             # -- output --
             cv2.namedWindow('input', cv2.WINDOW_NORMAL)
             cv2.namedWindow('edges', cv2.WINDOW_NORMAL)
 
             cv2.imshow('input', frame)
-            cv2.imshow('edges', edges)
 
         # stop
         if (cv2.waitKey(1) & 0xFF == ord('n')) or frame_counter == video.get(cv2.CAP_PROP_FRAME_COUNT)-1:
--- paintingDetection.py
+++ paintingDetection.py
@@ -1,49 +1,43 @@
-import numpy as np
 import cv2
 
-def paintingDetection(frame, video):
+def paintingDetection(frame):
 
-    _width = video.get(3)
-    _height = video.get(4)
-    _margin = 0.0
-    corners = np.array(
-        [
-            [[_margin, _margin]],
-            [[_margin, _height + _margin]],
-            [[_width + _margin, _height + _margin]],
-            [[_width + _margin, _margin]],
-        ]
-    )
+    blur = cv2.medianBlur(frame,21)
 
-    pts_dst = np.array(corners, np.float32)
+    frame1 = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
 
-    # gray
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.bilateralFilter(gray, 1, 10, 120)
+    #frame2= cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # edges
-    Th1 = 60  # cv2.getTrackbarPos("Th1", "Trackbars")
-    Th2 = 50  # cv2.getTrackbarPos("Th2", "Trackbars")
-    edges = cv2.Canny(gray, Th1, Th2)
+    Hl = cv2.getTrackbarPos("Hl", "Trackbars")
+    Sl = cv2.getTrackbarPos("Sl", "Trackbars")
+    Vl = cv2.getTrackbarPos("Vl", "Trackbars")
+    Hg = cv2.getTrackbarPos("Hg", "Trackbars")
+    Sg = cv2.getTrackbarPos("Sg", "Trackbars")
+    Vg = cv2.getTrackbarPos("Vg", "Trackbars")
 
-    # contours
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
-    closed = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
-    contours, h = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    mask = cv2.inRange(frame1, (Hl,Sl,Vl), (Hg, Sg, Vg))
 
-    # print contours
-    for cont in contours:
-        if cv2.contourArea(cont) > 5000:
-            arc_len = cv2.arcLength(cont, True)
-            approx = cv2.approxPolyDP(cont, 0.1 * arc_len, True)
-            if (len(approx) == 4):
-                IS_FOUND = 1
-                pts_src = np.array(approx, np.float32)
-                h, status = cv2.findHomography(pts_src, pts_dst)
-                out = cv2.warpPerspective(frame, h, (int(_width + _margin * 2), int(_height + _margin * 2)))
-                cv2.drawContours(frame, [approx], -1, (0, 255, 0), 3)
-            else:
-                pass
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    return edges, frame
+    for cnt in contours:
+        epsilon = 0.1 * cv2.arcLength(cnt, True)
+        approx = cv2.approxPolyDP(cnt, epsilon, True)
+        if len(approx) ==4:
+            if cv2.contourArea(approx) > 100000:
+                cv2.drawContours(blur, [approx], 0, (0, 255, 0), 3)
+    cv2.drawContours(blur, approx, -1, (0, 255, 0), 3)
 
+
+   # detected_circles = cv2.HoughCircles(mask, cv2.HOUGH_GRADIENT, 1, 20, param1=50,
+   #                                         param2=30, minRadius=500, maxRadius=900)
+   #
+   # if detected_circles is not None:
+   #     detected_circles = np.uint16(np.around(detected_circles))
+   #     for pt in detected_circles[0, :]:
+   #         a, b, r = pt[0], pt[1], pt[2]
+   #          Draw the circumference of the circle.
+   #         cv2.circle(frame, (a, b), r, (255, 255, 255), 2)
+
+    cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
+    cv2.imshow('frame', mask)
+    return  blur
