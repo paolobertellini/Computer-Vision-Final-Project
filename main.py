0a0d05923aab9babc1c4abc0faa6c284fc33cf0a
giuseppe <277168@studenti.unimore.it>
2020-04-24 11:25:51 +0200

    [Es 1]

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from paintingDetection import paintingDetection
 
 paolo_path = 'C:/VCS-project/VCS-project/Project material/videos/all/'
 dav_path = '/media/davide/aukey/progetto_vision/videos/'
+pepp_path = '/media/giuseppe/Volume/Peppe/Unimore/Vision and Cognitive Systems/Project material/videos/'
 
-path = dav_path
+path = pepp_path
 videos = os.listdir(path)
 
 # trackbars
@@ -31,13 +32,14 @@ for videoFile in videos:
         if frame is not None:
             frame_counter += 1
 
-            frame = paintingDetection(frame)
+            frame, thr = paintingDetection(frame)
 
             # -- output --
             cv2.namedWindow('input', cv2.WINDOW_NORMAL)
             cv2.namedWindow('edges', cv2.WINDOW_NORMAL)
 
             cv2.imshow('input', frame)
+            cv2.imshow('edges', thr)
 
         # stop
         if (cv2.waitKey(1) & 0xFF == ord('n')) or frame_counter == video.get(cv2.CAP_PROP_FRAME_COUNT)-1:
--- paintingDetection.py
+++ paintingDetection.py
@@ -2,30 +2,32 @@ import cv2
 
 def paintingDetection(frame):
 
-    blur = cv2.medianBlur(frame,21)
+    blur = cv2.medianBlur(frame,15)
+    #frame1 = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
 
-    frame1 = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+    frame2= cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    #frame2= cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Hl = cv2.getTrackbarPos("Hl", "Trackbars")
+    # Sl = cv2.getTrackbarPos("Sl", "Trackbars")
+    # Vl = cv2.getTrackbarPos("Vl", "Trackbars")
+    # Hg = cv2.getTrackbarPos("Hg", "Trackbars")
+    # Sg = cv2.getTrackbarPos("Sg", "Trackbars")
+    # Vg = cv2.getTrackbarPos("Vg", "Trackbars")
+    #
+    # mask = cv2.inRange(frame1, (Hl,Sl,Vl), (Hg, Sg, Vg))
+    # clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    # cl1 = clahe.apply(mask)
 
-    Hl = cv2.getTrackbarPos("Hl", "Trackbars")
-    Sl = cv2.getTrackbarPos("Sl", "Trackbars")
-    Vl = cv2.getTrackbarPos("Vl", "Trackbars")
-    Hg = cv2.getTrackbarPos("Hg", "Trackbars")
-    Sg = cv2.getTrackbarPos("Sg", "Trackbars")
-    Vg = cv2.getTrackbarPos("Vg", "Trackbars")
-
-    mask = cv2.inRange(frame1, (Hl,Sl,Vl), (Hg, Sg, Vg))
-
-    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    thr = cv2.adaptiveThreshold(frame2, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 17, 2)
+    contours, _ = cv2.findContours(thr, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     for cnt in contours:
         epsilon = 0.1 * cv2.arcLength(cnt, True)
         approx = cv2.approxPolyDP(cnt, epsilon, True)
         if len(approx) ==4:
-            if cv2.contourArea(approx) > 100000:
-                cv2.drawContours(blur, [approx], 0, (0, 255, 0), 3)
-    cv2.drawContours(blur, approx, -1, (0, 255, 0), 3)
+            if cv2.contourArea(approx) > 10000:
+                cv2.drawContours(frame, [approx], 0, (0, 255, 0), 3)
+    #cv2.drawContours(blur, approx, -1, (0, 255, 0), 3)
 
    # detected_circles = cv2.HoughCircles(mask, cv2.HOUGH_GRADIENT, 1, 20, param1=50,
    #                                         param2=30, minRadius=500, maxRadius=900)
@@ -37,6 +39,6 @@ def paintingDetection(frame):
    #          Draw the circumference of the circle.
    #         cv2.circle(frame, (a, b), r, (255, 255, 255), 2)
 
-    cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
-    cv2.imshow('frame', mask)
-    return  blur
+    # cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
+    # cv2.imshow('frame', mask)
+    return frame, thr
