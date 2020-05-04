5af2980071ac0effb0202f1dbd1f6b01c5e84a3c
paolobertellini <225184@studenti.unimore.it>
2020-05-04 12:33:46 +0200

    Painting detection

--- main.py
+++ main.py
@@ -10,22 +10,12 @@ videos = os.listdir(path)
 def nothing(x): pass
 
 cv2.namedWindow("Trackbars")
-cv2.createTrackbar("Hl", "Trackbars", 16, 180, nothing)
+cv2.createTrackbar("Hl", "Trackbars", 0, 180, nothing)
 cv2.createTrackbar("Sl", "Trackbars", 0, 255, nothing)
 cv2.createTrackbar("Vl", "Trackbars", 0, 255, nothing)
-cv2.createTrackbar("Hg", "Trackbars", 80, 180, nothing)
+cv2.createTrackbar("Hg", "Trackbars", 83, 180, nothing)
 cv2.createTrackbar("Sg", "Trackbars", 255, 255, nothing)
 cv2.createTrackbar("Vg", "Trackbars", 255, 255, nothing)
-
-cv2.namedWindow("Circles")
-k1 = cv2.createTrackbar("k1", "Circles", 1, 100, nothing)
-dist_centri = cv2.createTrackbar("dist_centri", "Circles", 40, 200, nothing)
-p1 = cv2.createTrackbar("p1", "Circles", 50, 200, nothing)
-false_circle = cv2.createTrackbar("false_circle", "Circles", 50, 200, nothing)
-minR = cv2.createTrackbar("minR", "Circles", 300, 1000, nothing)
-maxR = cv2.createTrackbar("maxR", "Circles", 500, 1000, nothing)
-th1 = cv2.createTrackbar("th1", "Circles", 1, 100, nothing)
-th2 = cv2.createTrackbar("th2", "Circles", 1, 100, nothing)
 
 for videoFile in videos:
 
@@ -37,20 +27,18 @@ for videoFile in videos:
         ret, frame = video.read()
 
         if frame is not None:
+
             frame_counter += 1
 
             if frame_counter % 1 == 0:
 
                 frame, mask = paintingDetection(frame)
 
-                # -- output --
+                # output
                 cv2.namedWindow('input', cv2.WINDOW_NORMAL)
                 cv2.namedWindow('mask', cv2.WINDOW_NORMAL)
-                # cv2.namedWindow('edges', cv2.WINDOW_NORMAL)
-
                 cv2.imshow('input', frame)
                 cv2.imshow('mask', mask)
-                # cv2.imshow('edges', edges)
 
         # stop
         if (cv2.waitKey(1) & 0xFF == ord('n')) or frame_counter == video.get(cv2.CAP_PROP_FRAME_COUNT)-1:
--- paintingDetection.py
+++ paintingDetection.py
@@ -1,13 +1,14 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
-from circleDetection import circleDetection
-
 
 def paintingDetection(frame):
 
     frame = cv2.erode(frame, None, iterations=2)
-    frame = cv2.dilate(frame, None, iterations=2)
+    # frame = cv2.dilate(frame, None, iterations=2)
+
+    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
     # frame = cv2.fastNlMeansDenoisingColored(frame, None, 10, 10, 7, 11)
     # frame = cv2.equalizeHist(frame)
     frame = cv2.medianBlur(frame, 13)
@@ -15,7 +16,6 @@ def paintingDetection(frame):
 
     # frame= cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-
 
     Hl = cv2.getTrackbarPos("Hl", "Trackbars")
     Sl = cv2.getTrackbarPos("Sl", "Trackbars")
@@ -30,19 +30,11 @@ def paintingDetection(frame):
 
     contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    # h = cv2.calcHist(frame, 3, mask, histSize, ranges[, hist[, accumulate]])
-
     for cnt in contours:
         epsilon = 0.1 * cv2.arcLength(cnt, True)
         approx = cv2.approxPolyDP(cnt, epsilon, True)
-        if len(approx) >= 4:
+        if len(approx) == 4:
             if cv2.contourArea(approx) > 5000:
                 cv2.drawContours(frame, [approx], 0, (0, 255, 0), 3)
-
-    # cv2.drawContours(frame, approx, -1, (0, 255, 0), 3)
-
-    # edges, frame = circleDetection(frame_gray)
-
-
 
     return frame, mask
